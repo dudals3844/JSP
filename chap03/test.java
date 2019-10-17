65eaf3a40e80233dc5c629e68b8d02f9e3f2dca1
Young Min <dudls3844@gmail.com>
2019-10-17 10:52:04 +0000

    learn setting error page using web.xml

--- chap03/test.java
+++ chap03/test.java
@@ -1,18 +1,9 @@
 package webapps.chap03;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URLEncoder;
-import java.sql.Struct;
-import java.util.Enumeration;
 
 
 public class test{
     public static void main(String[] args) {
-        Enumeration<String> attrEnum = application.getAttributeNames();
-        while (attrEnum.hasMoreElements()) {
-            String name = attrEnum.nextElement();
-            Object value = application.getAttribute(name);
-        }
+        System.out.println("Hi java");
     }
-}`
+}
---
+++ chap06/error/error404.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType= "text/html; charset=utf-8"%>
+
+<html>
+<head><title>404 error</title></head>
+<body>
+    주소를 올바르게 입력했는지 확인하십시오.
+</body>
+</html>
