570585c336a185d5f7ee73bc60230e36da30ada5
Young Min <dudls3844@gmail.com>
2019-11-03 08:35:37 +0000

    learn connection pool

--- chap14/WEB-INF/src/jdbc/MySQLDriverLoader.java
+++ chap14/WEB-INF/src/jdbc/MySQLDriverLoader.java
@@ -1,15 +1,15 @@
 package jdbc;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 
 public class MySQLDriverLoader extends HttpServlet {
+
     public void init(ServletConfig config) throws ServletException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception ex) {
-            //TODO: handle exception
+        } catch(Exception ex) {
             throw new ServletException(ex);
         }
     }
